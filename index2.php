d33d8c269cccefd37859c2f1109903f673913474
runoneall <runoobsteve@126.com>
2024-11-13 09:32:25 +0800

    update files

--- index2.php
+++
@@ -1,7 +0,0 @@
-
-<form action="admin-login.php" method="post" enctype="multipart/form-data">
-    进入后台
-    <input type="text" placeholder="用户名" id="UserName" name="UserName" required>
-    <input type="password" placeholder="密码" id="Password" name="Password" required>
-    <input type="submit" value="登录">
-</form>
