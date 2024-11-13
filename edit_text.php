bc001b783d38cf0066ad0adf6f905ea249b5a376
runoneall <runoobsteve@126.com>
2024-11-13 17:38:59 +0800

    update files

---
+++ edit_text.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+
+// imports
+require "config.php";
+require "tool.php";
+
+// set variable
+$config=readConfig();
+$permissions=$config['permissions'];
+$banned=$config['banned'];
+$root_dir=$config['rootFolder'];
+$current_dir=$_SESSION["current_dir"];
+$host=$_SESSION["host"];
+
+// check permission
+if ($permissions['fileEditor'] !== true) {
+    header('Location: /');
+    exit;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $file_path = $_POST["file-path"];
+    $content = $_POST["editor"];
+
+    // ban root edit
+    if (!is_have($file_path, $root_dir) || is_have($file_path, '..')) {
+        header('Location: /?dir='.$current_dir);
+        exit;
+    }
+
+    // check forbidden
+    $fileExtension = pathinfo($file_path, PATHINFO_EXTENSION);
+    if (in_array(strtolower($fileExtension), $banned)) {
+        header('Location: /?dir='.$current_dir);
+        exit;
+    }
+
+    // write & save file
+    $file = fopen($file_path, "w");
+    if ($file) {
+        fwrite($file, $content);
+        fclose($file);
+        header('Location: /text_editor.php?file='.$file_path);
+    } else {
+        header('Location: /?dir='.$current_dir);
+    }
+}
+?>
--- text_editor.php
+++ text_editor.php
@@ -52,11 +52,16 @@ header('Content-Type: text/html;charset=utf-8');
     </head>
     <body>
         <form action="edit_text.php" method="post">
+            <br>
+            <div class="file-info">
+                编辑:&nbsp;
+                <input type="text" id="file-path" name="file-path" value=<?php echo $file_path ?> readonly>
+            </div>
             <div class="editor-menu">
                 &nbsp;&nbsp;
-                <a id="unsave" href="/?dir=<?php echo $current_dir; ?>">不保存并退出</a>
+                <a id="unsave" href="/?dir=<?php echo $current_dir; ?>">退出</a>
                 &nbsp;&nbsp;
-                <input id="save" type="submit" value="保存并继续">
+                <input id="save" type="submit" value="保存">
             </div>
             <div class="editor-container">
                 <div class="line-numbers"></div>
